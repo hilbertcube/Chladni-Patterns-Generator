a160cce4a678b5e7e78409ea0333da8cbf4201a5
Don D. Le <ledongduu@gmail.com>
2025-08-14 16:58:05 -0700

    added vectorized version of square wave script

--- time_elapse/plot.py
+++ time_elapse/plot.py
@@ -7,13 +7,12 @@ plt.rcParams['font.family'] = 'serif'
 
 t = 1.569
 
-# 0.2 * np.cos(t) * (np.cos(2 * np.pi * x / 2) * np.cos(4 * np.pi * y /2) + np.cos(4 * np.pi * x/2) * np.cos(2 * np.pi * y/2))
 def f(x, y):
-    return np.sin(np.sqrt(x**2 + y**2))/np.sqrt(x**2 + y**2)
+    return 0.2 * np.cos(t) * (np.cos(2 * np.pi * x / 2) * np.cos(4 * np.pi * y /2) + np.cos(4 * np.pi * x/2) * np.cos(2 * np.pi * y/2))
 
 # mesh
-x = np.linspace(-16, 16, 500)
-y = np.linspace(-16, 16, 500)
+x = np.linspace(-1.0, 1.0, 500)
+y = np.linspace(-1.0, 1.0, 500)
 X, Y = np.meshgrid(x, y)
 Z = f(X, Y)
 
@@ -25,10 +24,12 @@ wireframe = ax.plot_wireframe(X, Y, Z, color='k', linewidth=0.1)
 
 ax.set_box_aspect([1,1,0.4])
 
-ax.set_xlabel(rf"$x$")
-ax.set_ylabel(rf"$y$")
-ax.set_zlabel(rf"$z$")
+ax.set_xlabel(rf"$x$", color='grey')
+ax.set_ylabel(rf"$y$", color='grey')
+ax.set_zlabel(rf"$z$", color='grey')
+ax.tick_params(axis='both', labelcolor='grey')
 #ax.set_title(rf"$t = 1$")
 #fig.colorbar(surface, shrink=0.5, aspect=5)
 
+plt.savefig('plot.png', transparent=True, dpi=300)
 plt.show()
